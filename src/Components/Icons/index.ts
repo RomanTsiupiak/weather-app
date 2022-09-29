3a7aada1edf0fd32b303d813a3593566acad23b8
Roman Tsiupiak <roman.ts@nerdysoft.com>
2022-09-29 15:31:49 +0300

    feat: add Icons components

---
+++ src/Components/Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CloudIcon } from './icon/CloudIcon';
+export { default as CloudSmallIcon } from './icon/CloudSmallIcon';
+export { default as DropIcon } from './icon/DropIcon';
+export { default as WindIcon } from './icon/WindIcon';
