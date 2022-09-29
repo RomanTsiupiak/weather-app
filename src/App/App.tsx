124231c0ca3fbb4db7c2ab538798793eff5a13ba
Roman Tsiupiak <roman.ts@nerdysoft.com>
2022-09-29 10:09:05 +0300

    feat: init commit

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">Weather Application</div>
+  );
+}
+
+export default App;
